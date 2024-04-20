c93a60422e10881d44fc90f2f9e79a0c0037fd5c
Michiel Boland <michiel@boland.org>
2024-04-20 20:00:02 +0200

    Use proper name for bootstrap-server

--- create-topic.sh
+++ create-topic.sh
@@ -1,1 +1,1 @@
-docker compose exec broker /opt/kafka/bin/kafka-topics.sh --create --topic streams-plaintext-input --bootstrap-server kafka:9092
+docker compose exec broker /opt/kafka/bin/kafka-topics.sh --create --topic streams-plaintext-input --bootstrap-server broker:9092
