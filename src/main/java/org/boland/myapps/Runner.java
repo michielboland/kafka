3d8be47824ca663a374cf5627401f89454756832
Michiel Boland <michiel@boland.org>
2024-04-20 20:01:52 +0200

    Exit on error

--- src/main/java/org/boland/myapps/Runner.java
+++ src/main/java/org/boland/myapps/Runner.java
@@ -3,36 +3,56 @@ package org.boland.myapps;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 
+import static org.apache.kafka.streams.errors.StreamsUncaughtExceptionHandler.StreamThreadExceptionResponse.SHUTDOWN_CLIENT;
+
 class Runner {
 
-    void runStream(StreamsBuilder builder, Properties props) {
+    private static final Logger LOG = LoggerFactory.getLogger(Runner.class);
+    private boolean close = true;
+
+    void runStream(@NotNull StreamsBuilder builder, Properties props) {
         final Topology topology = builder.build();
+        int status;
         try (KafkaStreams streams = new KafkaStreams(topology, props)) {
-            runStreams(streams);
+            status = runStreams(streams);
         }
+        close = false;
+        System.exit(status);
     }
 
-    private void runStreams(KafkaStreams streams) {
+    private int runStreams(@NotNull KafkaStreams streams) {
         final CountDownLatch latch = new CountDownLatch(1);
 
         // attach shutdown handler to catch control-c
         Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
             @Override
             public void run() {
-                streams.close();
-                latch.countDown();
+                if (close) {
+                    LOG.warn("Caught SIGINT - shutting down");
+                    streams.close();
+                    latch.countDown();
+                }
             }
         });
 
+        Thread runnerThread = Thread.currentThread();
+        streams.setUncaughtExceptionHandler(e -> {
+            runnerThread.interrupt();
+            return SHUTDOWN_CLIENT;
+        });
+        streams.start();
         try {
-            streams.start();
             latch.await();
-        } catch (Throwable e) {
-            System.exit(1);
+        } catch (InterruptedException e) {
+            return 1;
         }
+        return 0;
     }
 }
