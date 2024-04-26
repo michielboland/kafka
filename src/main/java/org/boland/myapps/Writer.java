64578ca27d52a520cff68e025558c23d3b4dda3c
Michiel Boland <michiel.boland@first8.nl>
2024-04-26 15:21:02 +0200

    Redo Writer (not tested yet)

--- src/main/java/org/boland/myapps/Writer.java
+++ src/main/java/org/boland/myapps/Writer.java
@@ -1,22 +1,14 @@
 package org.boland.myapps;
 
-import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.processor.PunctuationType;
-import org.apache.kafka.streams.processor.Punctuator;
 import org.apache.kafka.streams.processor.api.Processor;
 import org.apache.kafka.streams.processor.api.ProcessorContext;
 import org.apache.kafka.streams.processor.api.Record;
-import org.apache.kafka.streams.state.KeyValueIterator;
-import org.apache.kafka.streams.state.KeyValueStore;
-import org.apache.kafka.streams.state.Stores;
 import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.time.Duration;
+import java.util.Base64;
 import java.util.Properties;
+import java.util.Random;
 
 public class Writer {
 
@@ -26,60 +18,51 @@ public class Writer {
                 .defaultBootstrapServer()
                 .build();
         var builder = new StreamsBuilder();
-
-        builder.addStateStore(Stores.keyValueStoreBuilder(Stores.persistentKeyValueStore("dummyStore"), new Serdes.StringSerde(), new Serdes.StringSerde()));
         builder.<String, String>stream("dummy")
-                .process(DummyProcessor::new, "dummyStore")
+                .process(DummyProcessor::new)
                 .to("streams-plaintext-input");
         new Runner().runStream(builder, props);
     }
 
     private static class DummyProcessor implements Processor<String, String, String, String> {
 
-        private KeyValueStore<String, String> dummyStore;
+        private final Random random = new Random();
+        private final Base64.Encoder encoder = Base64.getEncoder();
+        private ProcessorContext<String, String> context;
 
         @Override
         public void init(@NotNull ProcessorContext<String, String> context) {
-            dummyStore = context.getStateStore("dummyStore");
-            context.schedule(Duration.ofSeconds(1), PunctuationType.WALL_CLOCK_TIME, new DummyPunctuator(context, dummyStore));
+            this.context = context;
         }
 
         @Override
         public void process(@NotNull Record<String, String> record) {
-            final String key;
-            if (record.key() == null && record.value() != null) {
-                key = record.value().substring(0, 4);
-            } else {
-                key = record.key();
+            String value = record.value();
+            if (value == null) {
+                return;
             }
-            dummyStore.put(String.valueOf(key), record.value());
-        }
-    }
-
-    private static class DummyPunctuator implements Punctuator {
-
-        private static final Logger LOG = LoggerFactory.getLogger(DummyPunctuator.class);
-        private final ProcessorContext<String, String> context;
-        private final KeyValueStore<String, String> dummyStore;
-
-        private DummyPunctuator(ProcessorContext<String, String> context, KeyValueStore<String, String> dummyStore) {
-            this.context = context;
-            this.dummyStore = dummyStore;
-        }
-
-        @Override
-        public void punctuate(long timestamp) {
-            KeyValue<String, String> keyValue;
-            try (KeyValueIterator<String, String> iter = dummyStore.all()) {
-                if (iter.hasNext()) {
-                    keyValue = iter.next();
-                } else {
-                    return;
-                }
+            String[] a = value.split(" ");
+            if (a.length != 2) {
+                return;
             }
-            LOG.atInfo().setMessage("outgoing").addKeyValue("key", keyValue.key).addKeyValue("value", keyValue.value).log();
-            context.forward(new Record<>(keyValue.key, keyValue.value, timestamp));
-            dummyStore.delete(keyValue.key);
+            long keyCount;
+            long valueCount;
+            try {
+                keyCount = Long.parseLong(a[0]);
+                valueCount = Long.parseLong(a[1]);
+            } catch (NumberFormatException e) {
+                return;
+            }
+            if (keyCount <= 0 || valueCount <= 0) {
+                return;
+            }
+            random.longs(valueCount, 0, keyCount).forEach(k -> context.forward(new Record<>(String.valueOf(k), randomString(), record.timestamp())));
+        }
+
+        private String randomString() {
+            byte[] bytes = new byte[15];
+            random.nextBytes(bytes);
+            return encoder.encodeToString(bytes);
         }
     }
 }
