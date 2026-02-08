5733c1e047a1602d77bdebeb25e2eaf0b9461c16
Michiel Boland <michiel@boland.org>
2026-02-08 11:59:46 +0100

    Do not crash if topic exists

--- src/main/java/org/boland/kafka/CreateTopics.java
+++ src/main/java/org/boland/kafka/CreateTopics.java
@@ -1,10 +1,12 @@
 package org.boland.kafka;
 
-import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.Admin;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.errors.TopicExistsException;
 
 import java.util.Arrays;
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
 public class CreateTopics {
@@ -17,10 +19,14 @@ public class CreateTopics {
             "streams-wordcount-output"
     };
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Properties props = Config.builder().defaultBootstrapServer().applicationId("create-topic").build();
-        try (AdminClient adminClient = AdminClient.create(props)) {
-            adminClient.createTopics(Arrays.stream(TOPICS).map(n -> new NewTopic(n, 2, (short) 1)).collect(Collectors.toList()));
+        try (var adminClient = Admin.create(props)) {
+            adminClient.createTopics(Arrays.stream(TOPICS).map(n -> new NewTopic(n, 2, (short) 3)).collect(Collectors.toList())).all().get();
+        } catch (ExecutionException e) {
+            if (!(e.getCause() instanceof TopicExistsException)) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
